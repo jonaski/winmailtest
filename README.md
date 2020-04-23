bf6c1c58f41cf578f08f6182f5fed4b5313c9b27
Jonas Kvinge <jonas@jkvinge.net>
2020-04-23 17:35:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+:mailbox: Win Mail Tester ![C/C++ CI](https://github.com/jonaski/winmailtest/workflows/C/C++%20CI/badge.svg)
+========================
+Win Mail Tester is a simple program to test MAPI and Outlook functionality in Windows written in C++.
+
+![Browse](https://raw.githubusercontent.com/jonaski/winmailtest/master/data/pictures/winmailtest.png)
---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+/*
+   Win Mail Tester
+   Copyright 2020, Jonas Kvinge <jonas@jkvinge.net>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+#include <glib.h>
+#include "windows.h"
+
+#include <QtGlobal>
+#include <QApplication>
+#include <QCoreApplication>
+#include <QLoggingCategory>
+
+#include "logging.h"
+#include "mainwindow.h"
+
+int main(int argc, char *argv[]) {
+
+  QCoreApplication::setApplicationName("winmailtest");
+  QCoreApplication::setOrganizationName("winmailtest");
+  QCoreApplication::setApplicationVersion("0.1.1");
+  QCoreApplication::setOrganizationDomain("jkvinge.net");
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+
+  logging::Init();
+
+  CoInitialize(nullptr);
+
+  QApplication app(argc, argv);
+
+  QLoggingCategory::defaultCategory()->setEnabled(QtDebugMsg, true);
+
+  Q_INIT_RESOURCE(data);
+  Q_INIT_RESOURCE(icons);
+
+  MainWindow w;
+
+  return app.exec();
+
+}
